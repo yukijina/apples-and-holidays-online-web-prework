4eec28fb27a5a162cd1631ccd103c68e46b73d9d
Yuki N <lunarbase@outlook.com>
2018-11-05 23:26:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -106,7 +106,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash each do |key, value|
+  holiday_hash.each do |key, value|
     value.each do |holiday, supply|
       if supply.include?("BBQ")
         holiday
