0bd77e8a1ff803eef22488af8e3cc3822f650f02
Yuki N <lunarbase@outlook.com>
2018-11-05 23:23:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -107,7 +107,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash each do |key, value|
-    
+    value.each do |holiday, supply|
+      supply.include?("BBQ")
 
 end
 
