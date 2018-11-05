2230bc65bd65533a97900bb8df9e21f89731cb28
Yuki N <lunarbase@outlook.com>
2018-11-05 22:52:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,8 +73,13 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   
+  holiday_hash.each |key, value|
+    if key == :winter
+      value.select do |holiday, supply|
+        supply
+      end
+    end
   
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
