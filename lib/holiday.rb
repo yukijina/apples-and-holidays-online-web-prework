f58280fc30df2084dfbc4d1eee10c64f98742b2a
Yuki N <lunarbase@outlook.com>
2018-11-05 23:04:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,6 +92,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_hash.each do |key, value|
+    value.each do |holiday, name|
+      puts
+    end
+  end
 
 end
 
